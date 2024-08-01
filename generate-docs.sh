d79e2eefd1959e0d274c2adbae90cd6883b133cf
karthik2804 <karthik.ganeshram@fermyon.com>
2024-08-01 03:21:16 +0200

    update docs
    Signed-off-by: karthik2804 <karthik.ganeshram@fermyon.com>

--- generate-docs.sh
+++ generate-docs.sh
@@ -2,17 +2,17 @@
 
 # Generate docs for v1
 # Todo: Update the branch name to the correct branch name
-git restore --source=upstream/main  spin-sdk 
+git restore --source=upstream/sdk-v1  spin-sdk 
 (cd spin-sdk && npm install && npm run build-docs)
 mv docs v1
 # Clean up docs after v1
-rm -rf v1/.nojekyll spin-sdk
+# rm -rf v1/.nojekyll spin-sdk
 
 # Generate docs for v2
 mkdir -p v2
 # Todo: Update the branch name to the correct branch name
-git --work-tree=./v2 checkout upstream/feat/sdk-v2 -- .
+git --work-tree=./v2 checkout fix_rdbms_types -- .
 (cd v2 && npm install && npm run build-docs)
 mv v2/docs ./
-rm -rf v2
+# rm -rf v2
 mv v1 docs
